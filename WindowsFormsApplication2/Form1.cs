e4761a7a1f11878dc54af4ab3c2ff204e6a805eb
Zheao Li <lizheao940510@gmail.com>
2015-11-06 20:54:30 +0800

    完成sound编码

--- WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,21 +20,34 @@ namespace WindowsFormsApplication2
 
         private void button1_Click(object sender, EventArgs e)
         {
-/*            OpenFileDialog openFile=new OpenFileDialog();
-            openFile.Filter = "mp3音频|*.mp3|wma音频|*.wma|3gpp音频|*.3gpp";
+            OpenFileDialog openFile=new OpenFileDialog();
+            openFile.Filter = "mp3音频|*.mp3|wav音频|*.wav|3gpp音频|*.3gpp";
             Base64 be=new Base64();
             byte[] bytes;
+            string resultConvert;
+            FileInfo fileInfo = null;
+            Encoding utf8 = Encoding.GetEncoding("utf-8");
+            Encoding gb2312 = Encoding.GetEncoding("gb2312");
             if (openFile.ShowDialog() == DialogResult.OK)
             {
                 bytes = be.loadWavSound(openFile.FileName);
-                string result = be.mp3Convert2Binary(bytes);
-                textBox1.Text = result;
+                resultConvert = be.mp3Convert2Binary(bytes);
+                fileInfo = new FileInfo(openFile.FileName);
+                AccessKey access = new AccessKey();
+                Sound sound = new Sound();
+                string json = access.JsonBuild();
+                string jsonTemp = access.JsonPost();
+                string accessKey = access.jsonReader(jsonTemp);
+                string jsonTemp1 = sound.JsonBuild(accessKey, resultConvert, Convert.ToInt32(fileInfo.Length));
+                string jsonTemp2 = sound.JsonPost(jsonTemp1);
+                byte[] utf81 = utf8.GetBytes(jsonTemp2);
+                byte[] gb2312b = Encoding.Convert(utf8, gb2312, utf81);
+               // string jsonFinal = sound.jsonReader(jsonTemp2);
+                textBox1.Text += gb2312.GetString(gb2312b);
+            }
 
-            }*/
-            AccessKey access=new AccessKey();
-            string json = access.JsonBuild();
-            string jsonTemp = access.JsonPost();
-            textBox1.Text=jsonTemp+"\r\n"+access.jsonReader(jsonTemp);
+
+            //textBox1.Text=jsonTemp+"\r\n"+access.jsonReader(jsonTemp);
         }
     }
 }
---
+++ WindowsFormsApplication2/Sound.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WindowsFormsApplication2
+{
+    class Sound
+    {
+        public string JsonBuild(string token,string data,int length)
+        {
+            StringWriter sw = new StringWriter();
+            JsonWriter writer = new JsonTextWriter(sw);
+            writer.WriteStartObject();
+            writer.WritePropertyName("format");
+            writer.WriteValue("wav");
+            writer.WritePropertyName("rate");
+            writer.WriteValue(8000);
+            writer.WritePropertyName("channel");
+            writer.WriteValue(1);
+            writer.WritePropertyName("cuid");
+            writer.WriteValue("8638110117331105");
+            writer.WritePropertyName("token");
+            writer.WriteValue(token);
+            writer.WritePropertyName("speech");
+            writer.WriteValue(data);
+            writer.WritePropertyName("len");
+            writer.WriteValue(length);
+            writer.WriteEndObject();
+            writer.Flush();
+            string result = sw.GetStringBuilder().ToString();
+            return result;
+        }
+
+        public string JsonPost(string Json)
+        {
+            //string fulldata = "grant_type=" + grant_type + "&client_id=" + client_id + "&client_secret=" + client_secret;
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] data = encoding.GetBytes(Json);
+            HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create("http://vop.baidu.com/server_api");
+            myRequest.Method = "POST";
+            myRequest.ContentType = "application/json";
+            myRequest.ContentLength = data.Length;
+            myRequest.Proxy = null;
+            Stream newStream = myRequest.GetRequestStream();
+            newStream.Write(data, 0, data.Length);
+            newStream.Close();
+            HttpWebResponse myResponse = (HttpWebResponse)myRequest.GetResponse();
+            StreamReader reader = new StreamReader(myResponse.GetResponseStream(), Encoding.Default);
+            string result = reader.ReadToEnd();
+            return result;
+        }
+        public string jsonReader(string json)
+        {
+            soundResult ak = JsonConvert.DeserializeObject<soundResult>(json);
+            return ak.ReultStrings();
+        }
+
+    }
+
+    public class soundResult
+    {
+        public string err_no { get; set; }
+        public string err_msg { get; set; }
+        public string sn { get; set; }
+        public string result { get; set; }
+        public  string ReultStrings()
+        {
+            return result;
+        }
+    }
+}
