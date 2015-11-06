66db6d7439de2c9fef274f92b32b5444fc654a56
Zheao Li <lizheao940510@gmail.com>
2015-11-06 15:23:26 +0800

    Base64 Encode

---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile=new OpenFileDialog();
+            openFile.Filter = "mp3音频|*.mp3|wma音频|*.wma|3gpp音频|*.3gpp";
+            Base64 be=new Base64();
+            byte[] bytes;
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                bytes = be.loadWavSound(openFile.FileName);
+                string result = be.mp3Convert2Binary(bytes);
+                textBox1.Text = result;
+
+            }
+        }
+    }
+}
---
+++ WindowsFormsApplication2/Server.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using SimpleJson;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    class Server
+    {
+
+
+    }
+}
